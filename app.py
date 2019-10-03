8b9b8c2eed129bd05738fa15310fcba7680f1d10
nitchakanbeam <51775061+nitchakanbeam@users.noreply.github.com>
2019-10-03 13:40:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ def callback():
     json_line = request.get_json()
     json_line = json.dumps(json_line)
     decoded = json.loads(json_line)
-    #userText= decoded["events"][0]['replyToken']
     user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']
+    userText = decoded['queryResult']['intent']['displayName']
     #userText= decoded["events"][0]['message"]['text']
     userText = decoded['queryResult']['intent']['displayName']
     #sendText(user,userText)
